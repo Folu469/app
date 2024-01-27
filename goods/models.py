6ece69994c291f3d4b00c7b86a45f43b5946ee72
Foliant469 <qwerty9087650850@gmail.com>
2024-01-27 23:56:43 +0300

    dsgfsdgdgdgs

--- goods/models.py
+++ goods/models.py
@@ -1,5 +1,5 @@
-from enum import unique
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -11,3 +11,17 @@ class Categories(models.Model):
         db_table = 'category'
         verbose_name = 'Категорию'
         verbose_name_plural = 'Категории'
+
+class Products(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name='Название')
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+    description = models.TextField(blank = True, null = True, verbose_name ='Описание')
+    # image = models.ImageField(upload_to='goods/images', blank = True, null = True)
+    # price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Цена')
+    
+    
+    
+    class Meta:
+        db_table = 'product'
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
